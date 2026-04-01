5729aea9426228161f7bb38ed68248263093423a
Alejandro Bedoya <abedoya28@students.claremontmckenna.edu>
2026-04-01 12:07:05 -0700

    removed dead code and fixed chat.py assistant

--- chat.py
+++ chat.py
@@ -7,56 +7,31 @@ load_dotenv()
 #non-class names (e.g. functions/variables) are in snake_case
 class Chat:
     client = Groq()
-    def __init__(self, message):
+    def __init__(self):
         self.messages = [        
                 {
                 #most important thign
                 "role": "system",
-                "content": "Write the output in 1-2 sentences."
+                "content": "Write the output in 1-2 sentences. Talk like a pirate."
             },
 
         ]
-    def send_message(self):
+    def send_message(self, message):
         self.messages.append(
-            {            {
+            {    
+                #system: never change; user: changes a lot;
+                # the message that you are sending to the AI      
                 "role": "user",
                 "content": message
+            }
         )
         chat_completion = self.client.chat.completions.create(
             messages = self.messages, 
             model="llama-3.1-8b-instant",
         )
         result = chat_completion.choices[0].message.content
-        messages.append({
-            "role": "assistent",
+        self.messages.append({
+            "role": "assistant",
             "content": result
         })
         return result
-
-'''
-client = Groq(
-    api_key=os.environ.get("GROQ_API_KEY"),  # This is the default and can be omitted
-)
-
-chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            #most important thign
-            "role": "system",
-            "content": "Write the output in 1-2 sentences."
-        },
-        {
-            "role": "user",
-            "content": "Explain the importance of low latency LLMs",
-        }
-    ],
-    #2nd most important thing to modify is the model; 
-    #diff models are good at different tasks
-    #it's tempting to alwasys use best model llama-3.1-8b-instant
-    #but that is bad practice
-    #use llama-3.1-8b-instant for debugging 
-    #llama-3.3-70b-versatile for final
-    model="llama-3.1-8b-instant",
-)
-print(chat_completion.choices[0].message.content)
-'''
